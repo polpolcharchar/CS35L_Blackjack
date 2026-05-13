738ff2f259a87dc6766bfb1e1cbf1884ca89aeb8
polpolcharchar <jackfitterer876@gmail.com>
2026-05-13 15:40:18 -0700

    Add display names to cards (eg. Jack for 11, queen for 12...

--- app/PlayingCard.jsx
+++ app/PlayingCard.jsx
@@ -9,10 +9,19 @@ export default function PlayingCard({suit, rank, faceup}){
         "s": "Spades"
     }
 
+    const getRankDisplayNameFromNumber = (input) => {
+        if(input >= 2 && input <= 10)return input;
+        if(input == 1)return "Ace";
+        if(input == 11)return "Jack";
+        if(input == 12)return "Queen";
+        if(input == 13)return "King";
+
+    }
+
     return (
         <div className="playingCard" style={{color: (suit === "h" || suit === "d" ? "red" : "black")}}>
             {
-                faceup === true && <p>{rank} of {suitMap[suit]}</p>
+                faceup === true && <p>{getRankDisplayNameFromNumber(rank)} of {suitMap[suit]}</p>
             }
         </div>
     )
